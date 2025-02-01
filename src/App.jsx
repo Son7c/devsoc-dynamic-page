c9b3a7be2a619965ced9d8ae2a003ce478b6f7b9
Aditya Sharma <adityasharma2005123@gmail.com>
2025-02-01 23:19:13 +0530

    Added the Error Page for Unknown Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import NewsSection from './components/NewsSection'
 import NewsListing from './components/NewsListing'
 import NewsDetails from './components/NewsDetails'
+import ErrorPage from './components/ErrorPage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<NewsSection />} />
         <Route path="/news-list" element={<NewsListing />} />
         <Route path="/news-details/:slug" element={<NewsDetails />} />
+        <Route path="*" element={<ErrorPage/>}/>
       </Routes>
     </BrowserRouter>
     </>
